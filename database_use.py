df4c16fd7ce8eb743e97fbd3cd5e717322f7a00b
ori <ori81510@gmail.com>
2020-12-10 18:17:01 +0200

    .....

--- database_use.py
+++ database_use.py
@@ -2,7 +2,8 @@ import database, models
 from playhouse.db_url import connect
 import os
 
-db = connect(os.environ.get('postgres://lxjtrjtfimkbzj:cd691f8132b9ca1aaa722074876098a3668b8a36a75b8b52eac64cc8b12fed83@ec2-3-218-123-191.compute-1.amazonaws.com:5432/d3aesmaed08di8')) 
+# db = models.database
+db = connect(os.environ.get('DATABASE_URL')) 
 database.create_tables(db)
 database.fill_easter_eggs(db)
 database.fill_achievements(db)
