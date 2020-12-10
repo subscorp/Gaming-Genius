ed1666c82202994e2df3342d58bb847e660df950
ori <ori81510@gmail.com>
2020-12-10 17:19:55 +0200

    Third commit

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ import random
 import json
 from html import unescape
 import os
+from playhouse.db_url import connect
 from models import Users, database, Leaderboard, EasterEggs, UserEasterEggs, Achievements, UserAchievements
 
 
 app = Flask(__name__)
-app.secret_key = os.urandom(32) 
+app.secret_key = os.urandom(32)
+db = connect(os.environ.get('postgres://lxjtrjtfimkbzj:cd691f8132b9ca1aaa722074876098a3668b8a36a75b8b52eac64cc8b12fed83@ec2-3-218-123-191.compute-1.amazonaws.com:5432/d3aesmaed08di8')) 
 
 
 def get_quiz():
