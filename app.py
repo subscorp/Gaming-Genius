252ea5b8447ce2f0455b892a273035febba29914
ori <ori81510@gmail.com>
2020-12-10 17:44:31 +0200

    .....

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from models import Users, database, Leaderboard, EasterEggs, UserEasterEggs, Ach
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
-db = connect(os.environ.get('postgres://lxjtrjtfimkbzj:cd691f8132b9ca1aaa722074876098a3668b8a36a75b8b52eac64cc8b12fed83@ec2-3-218-123-191.compute-1.amazonaws.com:5432/d3aesmaed08di8')) 
 
 
 def get_quiz():
